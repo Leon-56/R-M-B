dee0538f1987680b20be145fbdf05942fd508b24
Leon <963419094@qq.com>
2020-12-20 13:44:44 +0800

    Init project

---
+++ Source/RMB/RMB.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RMB.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RMB, "RMB" );
+ 
---
+++ Source/RMB/RMBGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RMBGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARMBGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ARMBGameMode();
+};
+
+
+
